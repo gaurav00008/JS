4a1415d44cd5b8e30b03856959fa985f3546e9f7
gaurav00008 <gauravpaneru95@gmail.com>
2026-04-15 20:25:35 +0545

    Async await

--- script.js
+++ script.js
@@ -239,3 +239,21 @@
 //     jokePara.innerText = data[0].text;
 // };
 
+
+
+
+
+/*ASYNC AWAIT EXAMPLES */
+
+function delay(){
+    return new Promise((resolve) => {
+        setTimeout(() => resolve("Done!"), 1000);
+    });
+}
+
+async function run() {
+    const result = await delay();
+    console.log(result);
+}
+
+run();
